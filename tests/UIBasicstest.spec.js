eab3f53485eaebe1bffd68a37a2d44253826ff65
Travis Carlson <traviter@gmail.com>
2026-07-15 20:04:53 -0700

    13 Test configuration file and first run

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,11 +1,11 @@
 import { test } from '@playwright/test';
 
-test('Browser Context Playwright test', async ({ browser, page }) => {
+test('Browser Context Playwright test', async ({ browser }) => {
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 });
 
-test('Page Playwright test', async ({ browser, page }) => {
-    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+test('Page Playwright test', async ({ page }) => {
+    await page.goto("https://google.com");
 });

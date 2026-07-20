4d83a7dc9fce4fbd73faa2a8532527f9f7befa3d
Travis Carlson <traviter@gmail.com>
2026-07-20 03:21:52 -0700

    42 Added configurable timeout tests

--- tests/AngularApp.spec.js
+++ tests/AngularApp.spec.js
@@ -9,13 +9,16 @@ test('Test form selection and submission using various accessors', async ({ page
     await page.getByLabel('Gender').selectOption("Female");
     await page.getByPlaceholder('Password').fill("PASSWORD123");
     await page.getByRole("button", { name: "Submit" }).click();
-    await expect(page.getByText("Success! The Form has been submitted successfully!.")).toBeVisible();
-    await page.getByRole("link", { name: "Shop" }).click();
+    await expect(page.getByText("Success! The Form has been submitted successfully!.")).toBeVisible({ timeout: 2_000 });
 });
 
 test('Test shop selection', async ({ page }) => {
+    const slowExpect = expect.configure({ timeout: 10_000 });
     await page.goto(pageUrl)
     await page.getByRole("link", { name: "Shop" }).click();
     await page.locator("app-card").filter({ hasText: 'Nokia Edge' }).getByRole("button").click();
+    await slowExpect(checkoutButton(page)).toContainText("Checkout ( 1 )");
 });
+
+const checkoutButton = (page) => page.locator('a.nav-link.btn.btn-primary');
 

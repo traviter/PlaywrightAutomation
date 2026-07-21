a9786adbd2e42a2761ef66c53fa26ab707334b69
Travis Carlson <traviter@gmail.com>
2026-07-21 06:41:49 -0700

    Clean up event booking test

--- tests/EventBooking.spec.js
+++ tests/EventBooking.spec.js
@@ -4,9 +4,6 @@ const baseUrl = "https://eventhub.rahulshettyacademy.com";
 const loginUrl = baseUrl;
 const bookingsUrl = baseUrl + "/bookings";
 
-const loginEmail = "traviter@gmail.com";
-const loginPassword = "8sx!vWjw54jJBAS";
-
 test('E2E Event Booking', async ({ page }) => {
     await login(page);
 
@@ -28,7 +25,9 @@ test('E2E Event Booking', async ({ page }) => {
     await navigateToEventCreation(page);
     await createEvent(page, event);
 
-    const seatsBeforeBooking = await getEventBookingCapacity(page, event, true);
+    const seatsBeforeBooking = await getEventBookingCapacity(page, event);
+
+    await navigateToBookEvent(page, event);
 
     const bookingRef = await bookEvent(page, {
         name: 'Travis Carlson',
@@ -38,10 +37,62 @@ test('E2E Event Booking', async ({ page }) => {
 
     await confirmBookingIsListedInHistory(page, event, bookingRef);
 
-    const seatsAfterBooking = await getEventBookingCapacity(page, event, false);
+    const seatsAfterBooking = await getEventBookingCapacity(page, event);
 
     expect(seatsBeforeBooking).toEqual(seatsAfterBooking + 1);
 });
+
+const login = async (page, credentials = { email: "traviter@gmail.com", password: "8sx!vWjw54jJBAS" }) => {
+    await page.goto(loginUrl);
+    await page.getByPlaceholder('you@email.com').fill(credentials.email);
+    await page.getByLabel('Password').fill(credentials.password);
+    await page.locator('#login-btn').click();
+    await expect(page.getByRole('link', { name: 'Browse Events →' })).toBeVisible();
+}
+
+const navigateToEventCreation = async (page, event) => {
+    // Navigate to the event management tab
+    await page.getByRole('button', { name: 'Admin' }).click();
+    await page.getByRole('navigation').getByRole('link', { name: 'Manage Events' }).click();
+}
+
+const createEvent = async (page, event) => {
+    await page.locator('#event-title-input').fill(event.title);
+    await page.locator('#admin-event-form textarea').fill(event.description);
+    await page.getByLabel('City').fill(event.city);
+    await page.getByLabel('Venue').fill(event.venue);
+    await page.getByLabel('Event Date & Time').fill(String(event.datetime));
+    await page.getByLabel('Price ($)').fill(String(event.price));
+    await page.getByLabel('Total Seats').fill(String(event.seats));
+    await page.locator('#add-event-btn').click();
+    await expect(page.getByText('Event created!')).toBeVisible();
+}
+
+const getEventBookingCapacity = async (page, event, book) => {
+    // Navigate to the events page listing
+    await page.getByTestId('nav-events').click();
+
+    // Wait for event cards to load
+    await expect(eventCards(page).first()).toBeVisible();
+
+    // Filter the events for the particular event
+    const targetEventCard = eventCard(page, event);
+
+    // Event doesn't load proper number of seats immediately.  It updates based off parsed bookings.
+    // I would probably classify this as a bug, but this allows the script to run and simulate actual user experience
+    await page.waitForTimeout(2_000);
+
+    // Parse out the number of remaining seats
+    const seatText = await targetEventCard.locator('span').filter({ hasText: 'seats available' }).innerText();
+
+    // Return the number of remaining seats
+    return parseInt(seatText, 10);
+}
+
+const navigateToBookEvent = async (page, event) => await eventCard(page, event).getByTestId('book-now-btn').click();
+
+const eventCards = (page) => page.getByTestId('event-card');
+const eventCard = (page, event) => eventCards(page).filter({ hasText: event.title });
 
 const bookEvent = async (page, attendee) => {
     // Ticket count should be 1 by default
@@ -63,89 +114,19 @@ const bookEvent = async (page, attendee) => {
     return (await bookingRefElement.innerText()).trim();
 }
 
-const getEventBookingCapacity = async (page, event, book) => {
-    // Navigate to the events page listing
-    await page.getByTestId('nav-events').click();
-
-    // Wait for event cards to load
-    const eventCards = page.getByTestId('event-card');
-    await expect(eventCards.first()).toBeVisible();
-
-    // Filter the events for the particular event
-    const targetEventCard = eventCards.filter({ hasText: event.title });
-
-    // Event doesn't load proper number of seats immediately.  It updates based off parsed bookings.
-    // I would probably classify this as a bug, but this allows the script to run and simulate actual user experience
-    await page.waitForTimeout(2_000);
-
-    // Parse out the number of remaining seats
-    const seatText = await targetEventCard.locator('span').filter({ hasText: 'seats available' }).innerText();
-
-    // Book the event
-    if (book) {
-        await targetEventCard.getByTestId('book-now-btn').click();
-    }
-
-    // Return the number of remaining seats
-    return parseInt(seatText, 10);
-}
-
-
 const confirmBookingIsListedInHistory = async (page, event, bookingRef) => {
+    // Navigate to view bookigns
     await page.getByRole('link', { name: 'View My Bookings' }).click();
 
+    // Confirm URL
     await expect(page).toHaveURL(bookingsUrl);
 
-    const allBookingCards = page.locator('#booking-card');
-
     // Expect all bookings are loaded
+    const allBookingCards = page.locator('#booking-card');
     await expect(allBookingCards.first()).toBeVisible();
 
     // Expect booking matching the reference number is present
     const targetBooking = allBookingCards.filter({ hasText: bookingRef });
     await expect(targetBooking).toBeVisible();
     await expect(targetBooking).toContainText(event.title);
-}
-
-
-test('test', async ({ page }) => {
-    if (true) {
-        return;
-    }
-    await page.getByRole('button', { name: 'Confirm Booking' }).click();
-    await expect(page.getByRole('heading', { name: 'Booking Confirmed! 🎉' })).toBeVisible();
-    await expect(page.getByText('Booking RefT-NFP8TD')).toBeVisible();
-    await expect(page.getByRole('main')).toContainText('T-NFP8TD');
-    await page.getByRole('button', { name: 'View My Bookings' }).click();
-    await expect(page.getByTestId('booking-card')).toBeVisible();
-    await expect(page.getByTestId('booking-card').getByRole('heading')).toContainText('Test Event 1784600354756');
-    await page.getByTestId('nav-events').click();
-    await expect(page.getByText('Dilli Diwali MelaTue, 20')).toBeVisible();
-    await expect(page.getByRole('main')).toContainText('49 seats available');
-});
-
-const navigateToEventCreation = async (page, event) => {
-    // Navigate to the event management tab
-    await page.getByRole('button', { name: 'Admin' }).click();
-    await page.getByRole('navigation').getByRole('link', { name: 'Manage Events' }).click();
-}
-
-const createEvent = async (page, event) => {
-    await page.locator('#event-title-input').fill(event.title);
-    await page.locator('#admin-event-form textarea').fill(event.description);
-    await page.getByLabel('City').fill(event.city);
-    await page.getByLabel('Venue').fill(event.venue);
-    await page.getByLabel('Event Date & Time').fill(String(event.datetime));
-    await page.getByLabel('Price ($)').fill(String(event.price));
-    await page.getByLabel('Total Seats').fill(String(event.seats));
-    await page.locator('#add-event-btn').click();
-    await expect(page.getByText('Event created!')).toBeVisible();
-}
-
-const login = async (page) => {
-    await page.goto(loginUrl);
-    await page.getByPlaceholder('you@email.com').fill(loginEmail);
-    await page.getByLabel('Password').fill(loginPassword);
-    await page.locator('#login-btn').click();
-    await expect(page.getByRole('link', { name: 'Browse Events →' })).toBeVisible();
 }
